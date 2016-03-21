6d5372c61db87afbc485a695205c5ef68ffbbbff
newsiberian <newsiberian2015@yandex.ru>
2016-03-21 19:22:59 +0600

    updated to Meteor 1.3-rc4

--- packages/reaction-i18n/package.js
+++ packages/reaction-i18n/package.js
@@ -14,7 +14,7 @@ Package.describe({
 //});
 
 Package.onUse(function (api) {
-  api.versionsFrom("METEOR@1.3-rc.3");
+  api.versionsFrom("METEOR@1.3-rc.4");
 
   // meteor base packages
   api.use("meteor-base");
--- packages/reaction-logging/package.js
+++ packages/reaction-logging/package.js
@@ -6,7 +6,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom("METEOR@1.3-rc.3");
+  api.versionsFrom("METEOR@1.3-rc.4");
 
   Npm.depends({
     "bunyan": "1.7.1",
